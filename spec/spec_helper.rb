8c5c9a845526829cece298a999771c010de65408
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-08 19:34:05 -0700

    Reconfigure rake version and shoulda matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,3 +31,11 @@ RSpec.configure do |config|
 
   # config.order = :random
 end
+
+# Needed to configure shoulda matchers, otherwise validate_presence_of returns NoMethodError.
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
