18c2a3bc2b032dec3999c11856d6877489aa6003
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-11 17:13:04 -0700

    Add test for or filtering organizations

--- spec/features/filtering_organizations_spec.rb
+++ spec/features/filtering_organizations_spec.rb
@@ -9,10 +9,26 @@ describe "Filtering organizations" do
 
     expect(page).to have_content(organization.name)
     expect(page).to have_content(other_organization.name)
-    
+
     check(1)
     click_button("Apply Filter")
     expect(page).to have_content(organization.name)
     expect(page).not_to have_content(other_organization.name)
+  end
+
+  scenario "or filter" do
+    organization = FactoryGirl.create(:organization)
+    other_organization = FactoryGirl.create(:organization, :update_eligibility)
+
+    visit root_path
+
+    expect(page).to have_content(organization.name)
+    expect(page).to have_content(other_organization.name)
+
+    check(1)
+    check(2)
+    click_button("Apply Filter")
+    expect(page).to have_content(organization.name)
+    expect(page).to have_content(other_organization.name)
   end
 end
