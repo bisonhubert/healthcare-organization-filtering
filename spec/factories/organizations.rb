a2d880de15248cc5190d78794798fc6776c19e96
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-11 17:10:56 -0700

    Add test for basic filter

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -2,6 +2,10 @@ FactoryGirl.define do
   factory :organization do
     sequence(:name) { |i| "Organization #{i}" }
     description "This is a generic description."
-    eligibilities ["Generic Eligibility Property"]
+    eligibilities ["Youth"]
+
+    trait :update_eligibility do
+      eligibilities ["LGBT"]
+    end
   end
 end
---
+++ spec/features/filtering_organizations_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "Filtering organizations" do
+  scenario "basic filter" do
+    organization = FactoryGirl.create(:organization)
+    other_organization = FactoryGirl.create(:organization, :update_eligibility)
+
+    visit root_path
+
+    expect(page).to have_content(organization.name)
+    expect(page).to have_content(other_organization.name)
+    
+    check(1)
+    click_button("Apply Filter")
+    expect(page).to have_content(organization.name)
+    expect(page).not_to have_content(other_organization.name)
+  end
+end
