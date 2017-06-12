8da92de461e9693ecf31a1e45b858ef1b2366905
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-12 13:04:41 -0700

    Clean up filtering organizations object

--- app/services/filter_organization.rb
+++ app/services/filter_organization.rb
@@ -1,30 +1,22 @@
 class FilterOrganization
-  def initialize(params)
+  def initialize(params = {})
     @organizations = params[:organizations]
     @eligibilities = params[:eligibilities]
     @query_type = params[:q]
   end
 
   def call
-    @query_type == 'inclusive' ? inclusive_filter : exclusive_filter
+    @query_type == "inclusive" ? inclusive_filter : exclusive_filter
   end
 
   private
     
   def inclusive_filter
-    filtered_organizations = []
-    @organizations.each do |organization|
-      organization.eligibilities.each do |eligibility|
-        if @eligibilities.include?(eligibility)
-          filtered_organizations.push(organization)
-        end
-      end
-    end
-    filtered_organizations
+    @organizations.select { |org| (org.eligibilities & @eligibilities).any? }
   end
 
 
   def exclusive_filter
-    @organizations.select { |org| org[:eligibilities] & @eligibilities == @eligibilities }
+    @organizations.select { |org| org.eligibilities & @eligibilities == @eligibilities }
   end
 end
