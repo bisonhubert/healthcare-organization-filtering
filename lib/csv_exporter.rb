8838443d3ca87cb187bf6792bf97d1a4c5957e89
Bison Hubert <davidbisonhubert@gmail.com>
2017-06-13 18:15:05 -0700

    Extract attributes from elegibilities & locations

--- lib/csv_exporter.rb
+++ lib/csv_exporter.rb
@@ -15,8 +15,26 @@ class CsvExporter
       @organizations = Organization.filter_organizations({eligibilities: @eligibility})
       csv << ["ID", "Name", "Description", "Eligibilities", "Locations"]
       @organizations.each do |org|
-        csv << [org.id, org.name, org.description, org.eligibilities, org.locations]
+        eligibilities = get_elegibilities(org.eligibilities)
+        locations = get_location_info(org.locations)
+        csv << [org.id, org.name, org.description, eligibilities, locations]
       end
+    end
+  end
+
+  def get_elegibilities(elegibilities)
+    elegibilities.map(&:name)
+  end
+
+  def get_location_info(locations)
+    locations.map do |location| 
+      {
+        name: location.name, 
+        address: location.address, 
+        city: location.city, 
+        state: location.state, 
+        zipcode: location.zipcode 
+      }
     end
   end
 end
